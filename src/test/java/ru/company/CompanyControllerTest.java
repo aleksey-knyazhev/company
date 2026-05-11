b2b19e335f0c066b99bf600df8c7acf261645215
aleksey-knyazhev <aleksey.knyazhev@gmail.com>
2026-05-11 11:57:45 +0300

    First Commit

--- src/test/java/ru/company/CompanyControllerTest.java
+++ src/test/java/ru/company/CompanyControllerTest.java
@@ -8,14 +8,12 @@ import io.micronaut.http.client.annotation.Client;
 import io.micronaut.test.extensions.junit5.annotation.MicronautTest;
 import jakarta.inject.Inject;
 import org.junit.jupiter.api.Test;
-import org.testcontainers.junit.jupiter.Testcontainers;
 
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 @MicronautTest(environments = "test")
-@Testcontainers
 class CompanyControllerTest {
 
     @Inject
